ae2e21a1922b3ee449a189af58aa594d54625120
jomarz <jorgemartinez05@gmail.com>
2015-02-22 19:40:40 -0500

    tidy project

--- run_analysis.R
+++ run_analysis.R
@@ -12,36 +12,51 @@ pathSubTest <- "UCI HAR Dataset/test/subject_test.txt"
 activitiesPath <- "UCI HAR Dataset/activity_labels.txt"
 namesPath <- "UCI HAR Dataset/features.txt"
 
-## We then read the files
+## We read the files into objects
 train <- read.table(pathTrain)
-tainActs <- read.table(pathActTrain)
+trainActs <- read.table(pathActTrain)
 trainSubjects <- read.table(pathSubTrain)
 test <- read.table(pathTest)
 testActs <- read.table(pathActTest)
 testSubjects <- read.table(pathSubTest)
 varNames <- read.table(namesPath)
+actLabels <- read.table(activitiesPath)
 
-## merge all the train data horizontally
-train <- cbind(train, tainActs, trainSubjects)
+## Set the column names of the Activities and Subjects vectors
+colnames(trainActs)[1] <- "Activity"
+colnames(trainSubjects)[1] <- "Subject"
+colnames(testActs)[1] <- "Activity"
+colnames(testSubjects)[1] <- "Subject"
 
-## merge all the test data horizontally
-test <- cbind(test, testActs, testSubjects)
+## Set the variable names from the features.txt file as the column names
+colnames(train) <- as.character(varNames$V2)
+colnames(test) <- as.character(varNames$V2)
 
-## subset the train and test data frames taking only the means and standard deviations
+## Subset the train and test data frames taking only the means and standard deviations
 ## variables we do this by subseting taking only the columns that correspond to variable 
-## names containing -mean()- and -std()-
-train <- train[,grep("-mean\\(\\)-|-std\\(\\)-",varNames$V2)]
-test <- test[,grep("-mean\\(\\)-|-std\\(\\)-",varNames$V2)]
+## names containing "mean()" and "std()"
+trainSub <- train[,grep("mean\\(\\)|std\\(\\)",colnames(train))]
+testSub <- test[,grep("mean\\(\\)|std\\(\\)",colnames(test))]
+
+## Merge all the train data horizontally
+trainSub <- cbind(trainSub, trainActs, trainSubjects)
+## Merge all the test data horizontally
+testSub <- cbind(testSub, testActs, testSubjects)
 
 ## We merge the two datasets by rows (vertically one after the other)
-mergedDF <- rbind(train, test)
+mergedDF <- rbind(trainSub, testSub)
 
-## set the variable names from the features.txt file as the column names
-## in the merged data set, first taking only the means and stds
-variables <- (varNames$V2[varNames$V2==grep("-mean\\(\\)-|-std\\(\\)-",varNames$V2),2])
-colnames(mergedDF) <- c(variables, "Activity", "Subject")
+## Name the activity in each observation according to activity_labels.txt
+mergedDF$Activity <- sapply(mergedDF$Activity, function(x) actLabels[actLabels$V1==x, 2])
 
-head(mergedDF)
+library(reshape2)
+
+nbr <- length(colnames(mergedDF))-2
+
+dataMelt <- melt(mergedDF, id=c("Subject", "Activity"), measure.vars=colnames(mergedDF)[c(1:nbr)])
+
+## Summarize taking the mean for each combination of Subject, Activity and variable
+tidy <- dcast(dataMelt, Subject + Activity ~ variable, mean)
 
 #We now export the merged data frame to a text file
-write.table(mergedDF, "assignment2tidy.txt", row.name=FALSE)
+write.table(tidy, "assignment2tidy.txt", row.name=FALSE)
